4f34b9258c3676d4a718aa95c77fcaa59f43454a
Roman E <livcey@gmail.com>
2019-06-24 11:12:26 +0300

    Update Digispot-Download.ps1

--- Digispot-Download.ps1
+++ Digispot-Download.ps1
@@ -44,7 +44,7 @@ New-Item -Path $v -Force -ItemType Directory | Out-Null
 
 $v1 = $v.Replace(".","-")
 Write-Host "Changes.html" -BackgroundColor Gray -ForegroundColor Black
-Invoke-WebRequest https://redmine.digispot.ru/projects/digispot/wiki/%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9_%D0%B2_%D0%BF%D0%BE%D0%BA%D0%BE%D0%BB%D0%B5%D0%BD%D0%B8%D0%B8_$v1 -OutFile Changes_$v1.html
+Invoke-WebRequest https://redmine.digispot.ru/projects/digispot/wiki/%D0%98%D1%81%D1%82%D0%BE%D1%80%D0%B8%D1%8F_%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D0%B9_%D0%B2_%D0%BF%D0%BE%D0%BA%D0%BE%D0%BB%D0%B5%D0%BD%D0%B8%D0%B8_$v1 -OutFile $v\Changes_$v1.html
 
 $files = @(
     "djinsetup.exe";
